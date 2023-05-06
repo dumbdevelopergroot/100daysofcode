f47b34b8dedcd5b6371618ca599a26aa2443f2b6
dumbdevelopergroot <thedumbdeveloper@hotmail.com>
2023-05-06 14:00:35 +0530

    day 5 beginning

---
+++ python/day5/ex1-avgheight.py
@@ -0,0 +1,15 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+sum = 0
+count = 0
+for a in student_heights:
+    sum = sum + a
+    count = count + 1
+print(round(sum/count))
+
---
+++ python/day5/notes.py
@@ -0,0 +1,10 @@
+#For Loop with Lists
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+  print(fruit)
+  print(fruit + " Pie")
+print(fruits)
+for number in range(1,10):
+    print(number)
+for number in range(1,10,3):#here the last argument 3 specifies the step..
+    print(number)#so output is 1,4,7(with 3 step)
